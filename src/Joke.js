c11d4710541da4352c7939400612501cafab2c10
unknown <jreidmke@gmail.com>
2020-12-01 14:14:38 -0600

    completed Joke component reformat

--- src/Joke.js
+++ src/Joke.js
@@ -1,31 +1,6 @@
 import React, { Component } from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {//props will be put in constructor
-  const upVote = () => vote(id, +1); //upVote method will be bound in reformat
-  const downVote = () => vote(id, -1);//downVote method will be bound in reformat
-
-  return (//return will start with render() func
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
-
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
-
-        {votes}
-      </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
-}
-
-export default Joke;
-
 class Joke extends Component {
   constructor(props) {
     super(props);
@@ -41,5 +16,49 @@ class Joke extends Component {
     this.props.vote(this.props.id, -1);
   }
 
+  render() {
+    return(
+      <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={this.upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
 
+        <button onClick={this.downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {this.props.votes}
+      </div>
+
+      <div className="Joke-text">{this.props.text}</div>
+    </div>
+    )
+  }
 }
+
+export default Joke;
+
+
+// function Joke({ vote, votes, text, id }) {//props will be put in constructor
+//   const upVote = () => vote(id, +1); //upVote method will be bound in reformat
+//   const downVote = () => vote(id, -1);//downVote method will be bound in reformat
+
+//   return (//return will start with render() func
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
